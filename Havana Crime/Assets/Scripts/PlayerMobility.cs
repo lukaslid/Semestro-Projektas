f1f4d481ff378425407d5df87419c25f71f29889
Vilius-PC\Vilius <kerpauskasss@gmail.com>
2018-03-06 21:21:39 +0200

    PLA1 - Added keyboard control; PLA2 - Added mouse control.

---
+++ Havana Crime/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //mouse control
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        //Movement for vertical/horizotal axis
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
