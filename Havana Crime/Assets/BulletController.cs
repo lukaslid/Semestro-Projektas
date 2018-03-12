6dca6822d96dddfd21f4b45db8866aaff87a24e7
Vilius-PC\Vilius <kerpauskasss@gmail.com>
2018-03-12 20:16:51 +0200

    WPN1, WPN2, WPN3, WPN4 tasks completed, added weapon and projectiles objects.

---
+++ Havana Crime/Assets/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    public float destroyTimer;
+
+	void Update () {
+        Destroy(this.gameObject, destroyTimer);
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Untagged")
+            Destroy(this.gameObject);
+
+    }
+}
