2db320056ae3a12e813700b84e68bdf8d9b2bb80
kestv <kestutis.ramulionis@gmail.com>
2018-03-13 23:39:45 +0200

    PLA5 quick fix for animations

--- Havana Crime/Assets/Scripts/WeaponScript.cs
+++ Havana Crime/Assets/Scripts/WeaponScript.cs
@@ -7,24 +7,49 @@ public class WeaponScript : MonoBehaviour {
     public Rigidbody2D projectilePrefab;
     public float fireRate;
     public float projectileSpeed;
+    public int bulletCount;
 
     private Transform firePoint;
     private float cooldown;
 
+    private Animator anim;
     private void Awake()
     {
         firePoint = transform.FindChild("FirePoint");
         if (firePoint == null)
             Debug.LogError("No FirePoint");
+        anim = GetComponentInParent<Animator>();
     }
 
     void Update()
     {
         if (Input.GetButton("Fire1") && Time.time > cooldown)
         {
-            cooldown = Time.time + (1 / fireRate);
-            Fire();
+            if (bulletCount > 0)
+            {
+                cooldown = Time.time + (1 / fireRate);
+                Fire();
+                bulletCount--;
+                
+            }
+            else
+            {
+                StartCoroutine(reload());
+            }
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(reload());
+        }
+    }
+
+    IEnumerator reload()
+    {
+        anim.SetBool("isAmmoEmpty", true);
+        yield return new WaitForSeconds(0.55f);
+        anim.SetBool("isAmmoEmpty", false);
+        bulletCount = 50;
+        
     }
 
     void Fire()
