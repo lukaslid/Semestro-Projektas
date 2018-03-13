b19d9b71083ef04760fbe6b5ea4bec1b47d32ebe
Vilius <kerpauskasss@gmail.com>
2018-03-14 00:16:45 +0200

    Merge branch 'master' of https://github.com/lukaslid/Semestro-Projektas
    # Conflicts:
    #	Havana Crime/Assets/Main.unity
    #	Havana Crime/Assets/Scripts/WeaponScript.cs

---
+++ Havana Crime/Assets/Scripts/WeaponScript_BASE_2396.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public Rigidbody2D projectilePrefab;
+    public float fireRate;
+    public float projectileSpeed;
+
+    private Transform firePoint;
+    private float cooldown;
+
+    private void Awake()
+    {
+        firePoint = transform.FindChild("FirePoint");
+        if (firePoint == null)
+            Debug.LogError("No FirePoint");
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > cooldown)
+        {
+            cooldown = Time.time + (1 / fireRate);
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        Vector3 firePointPosition = new Vector3(firePoint.position.x, firePoint.position.y, firePoint.position.z);
+        Rigidbody2D projectile = Instantiate(projectilePrefab, firePointPosition, firePoint.rotation);
+        projectile.AddForce(transform.up * projectileSpeed);
+
+    }
+}
---
+++ Havana Crime/Assets/Scripts/WeaponScript_REMOTE_2396.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour {
+
+    public Rigidbody2D projectilePrefab;
+    public float fireRate;
+    public float projectileSpeed;
+    public int bulletCount;
+
+    private Transform firePoint;
+    private float cooldown;
+
+    private Animator anim;
+    private void Awake()
+    {
+        firePoint = transform.FindChild("FirePoint");
+        if (firePoint == null)
+            Debug.LogError("No FirePoint");
+        anim = GetComponentInParent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > cooldown)
+        {
+            if (bulletCount > 0)
+            {
+                cooldown = Time.time + (1 / fireRate);
+                Fire();
+                bulletCount--;
+                
+            }
+            else
+            {
+                StartCoroutine(reload());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(reload());
+        }
+    }
+
+    IEnumerator reload()
+    {
+        anim.SetBool("isAmmoEmpty", true);
+        yield return new WaitForSeconds(0.55f);
+        anim.SetBool("isAmmoEmpty", false);
+        bulletCount = 50;
+        
+    }
+
+    void Fire()
+    {
+        Vector3 firePointPosition = new Vector3(firePoint.position.x, firePoint.position.y, firePoint.position.z);
+        Rigidbody2D projectile = Instantiate(projectilePrefab, firePointPosition, firePoint.rotation);
+        projectile.AddForce(transform.up * projectileSpeed);
+
+    }
+}
