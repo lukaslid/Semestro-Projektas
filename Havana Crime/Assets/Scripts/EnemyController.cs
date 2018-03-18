251f98986afc5c30039a908242b958184686ad64
lukaslid <lukaslid@gmail.com>
2018-03-18 15:29:07 +0200

    Completed ENE1, ENE2 tasks

---
+++ Havana Crime/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float movementSpeed;
+	public PlayerMobility player;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		player = FindObjectOfType<PlayerMobility> ();
+		
+	}
+	void FixedUpdate() {
+		rb.velocity = (transform.forward * movementSpeed);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt (player.transform.position);
+	}
+}
