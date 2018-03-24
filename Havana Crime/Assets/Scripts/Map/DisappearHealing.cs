3300aa55e89011295f868fd724b957b4b49ed3f1
Karolis <butnoriusk@gmail.com>
2018-03-24 21:43:43 +0200

    MAP3:added medicine box that player can pick up and heal + added boxes that destroyable only with weapon

---
+++ Havana Crime/Assets/Scripts/Map/DisappearHealing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisappearHealing : MonoBehaviour
+{
+    public float heal;
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            Destroy(this.gameObject, 0.2f);
+            col.SendMessage("HealDamage", heal);
+        }
+    }
+}
