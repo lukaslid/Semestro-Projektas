f75b3c18935c61594cae7775027bb6f1a155c81e
Vilius <kerpauskasss@gmail.com>
2018-04-07 23:39:41 +0300

    PLA8, WPN9, HUD3 completed; PLA8 - Weapon switch option WPN9 - Added different weapons (rifle, pistol, shotgun); HUD3 - Current weapon in use displayed on the screen.

---
+++ Havana Crime/Assets/Scripts/Weapon/WeaponScriptShotgun.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponScriptShotgun : MonoBehaviour {
+
+    public Rigidbody2D projectilePrefab;
+    public float fireRate;
+    public float projectileSpeed;
+    private Transform firePoint;
+    private float cooldown;
+
+    //Bullet management and reloading
+    private int bulletCount;
+    public int bulletMax;
+    public Text bulletText;
+    private bool isReloading = false;
+    private Animator anim;
+
+    private void Awake()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+            Debug.LogError("No FirePoint");
+        anim = GetComponentInParent<Animator>();
+    }
+
+    private void Start()
+    {
+        bulletCount = bulletMax;
+        SetBulletText();
+    }
+
+    //For future implementations (Weapon swap)
+    //private void OnEnable()
+    //{
+    //    isReloading = false;
+    //    anim.SetBool("isAmmoEmpty", false);
+    //}
+
+    void Update()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (bulletCount <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time > cooldown)
+        {
+            cooldown = Time.time + (1 / fireRate);
+            bulletCount--;
+            anim.SetBool("isShooting", true);
+            Fire();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && bulletCount != bulletMax)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        SetBulletText();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!Input.GetButton("Fire1")) anim.SetBool("isShooting", false);
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        anim.SetBool("isAmmoEmpty", true);
+        yield return new WaitForSeconds(0.55f);
+        anim.SetBool("isAmmoEmpty", false);
+        bulletCount = bulletMax;
+        SetBulletText();
+        yield return new WaitForSeconds(0.25f);
+        isReloading = false;
+    }
+
+    void Fire()
+    {
+        Vector3 firePointPosition = new Vector3(firePoint.position.x, firePoint.position.y, firePoint.position.z);
+        Rigidbody2D projectile = Instantiate(projectilePrefab, firePointPosition, firePoint.rotation);
+        Rigidbody2D projectile2 = Instantiate(projectilePrefab, firePointPosition, firePoint.rotation);
+        Rigidbody2D projectile3 = Instantiate(projectilePrefab, firePointPosition, firePoint.rotation);
+        projectile.AddForce(transform.up * projectileSpeed);
+        projectile2.AddForce(transform.up * projectileSpeed);
+        projectile3.AddForce(transform.up * projectileSpeed);
+
+    }
+
+    void SetBulletText()
+    {
+        bulletText.text = "Ammo: " + bulletCount;
+    }
+}
---
+++ Havana Crime/Assets/Scripts/Weapon/WeaponSwitching.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSwitching : MonoBehaviour {
+
+    public int selectedWeapon = 0;
+    public Text currentWeapon;
+    public string[] weaponList = { "Rifle", "Pistol", "Shotgun" };
+
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    void Update()
+    {
+        int previouslySelectedWeapon = selectedWeapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0;
+            }
+            else
+            {
+                selectedWeapon++;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                selectedWeapon--;
+            }
+        }
+
+        if(previouslySelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if(i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+        SetCurrentWeaponText();
+    }
+
+    void SetCurrentWeaponText()
+    {
+        currentWeapon.text = weaponList[selectedWeapon];
+    }
+}
