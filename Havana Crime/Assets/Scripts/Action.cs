aeaaae34cced60399ad5f7c4773fae5cdacadb10
kestv <kestutis.ramulionis@gmail.com>
2018-04-10 16:47:03 +0300

    SOU1 - added bg sound

--- Havana Crime/Assets/Scripts/Action.cs
+++ Havana Crime/Assets/Scripts/Action.cs
@@ -7,11 +7,21 @@ public class Action : MonoBehaviour {
 
 	public void Restart()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Main");
     }
 
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene("Options");
     }
 }
---
+++ Havana Crime/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour {
+    AudioSource asource;
+	// Use this for initialization
+	void Start () {
+        asource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
