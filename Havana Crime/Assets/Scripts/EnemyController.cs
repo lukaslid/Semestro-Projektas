068c0d6085ee8fe793117f70431d4de91c89fcd2
Karolis <butnoriusk@gmail.com>
2018-05-06 20:57:27 +0300

    EFF2 - completed, appears blood when hit enemy, EFF4 - in progress, appears damage number every time in same place

--- Havana Crime/Assets/Scripts/EnemyController.cs
+++ Havana Crime/Assets/Scripts/EnemyController.cs
@@ -2,42 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
 
-	private GameObject enemy;
-	private Pathfinding.AIPath pathfinding;
-	public float movementSpeed;
-	public GameObject player;
-	public bool reachable;
-	public float timeleft = 1.0f;
-	public float range = 2f;
+    private GameObject enemy;
+    private Pathfinding.AIPath pathfinding;
+    public float movementSpeed;
+    public GameObject player;
+    public bool reachable;
+    public float timeleft = 1.0f;
+    public float range = 2f;
+    public GameObject damageNumber;
+    public GameObject blood;
 
-	// Use this for initialization
-	void Start () {
-		Physics.IgnoreLayerCollision(this.gameObject.layer, this.gameObject.layer);
-		reachable = false;
-		player = GameObject.FindGameObjectWithTag ("Player");
-		enemy = gameObject;
-		pathfinding = enemy.GetComponent<Pathfinding.AIPath>();
-		enemy.GetComponent<Pathfinding.AIDestinationSetter> ().target = player.transform;
-	}
-		
-	// Update is called once per frame
-	void Update () {
-		if (! pathfinding.reachedEndOfPath) {
-			// move
-			pathfinding.canMove = true;
-			timeleft = 0.5f;
-		}
-		else {
-			//attack
-			pathfinding.canMove = false;
-			timeleft -= Time.deltaTime;
-			if (timeleft <= 0.0f) 
-			{
-				player.GetComponent<HealthBar> ().TakeDamage (100f);
-				timeleft = 1.5f;
-			}
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Physics.IgnoreLayerCollision(this.gameObject.layer, this.gameObject.layer);
+        reachable = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = gameObject;
+        pathfinding = enemy.GetComponent<Pathfinding.AIPath>();
+        enemy.GetComponent<Pathfinding.AIDestinationSetter>().target = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pathfinding.reachedEndOfPath)
+        {
+            // move
+            pathfinding.canMove = true;
+            timeleft = 0.5f;
+        }
+        else
+        {
+            //attack
+            pathfinding.canMove = false;
+            timeleft -= Time.deltaTime;
+            if (timeleft <= 0.0f)
+            {
+                player.GetComponent<HealthBar>().TakeDamage(100f);
+                timeleft = 1.5f;
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Bullet"))
+        {
+            Instantiate(blood, transform.position, Quaternion.identity);
+            Instantiate(damageNumber, transform.position, Quaternion.identity);
+        }
+    }
 }
+
---
+++ Havana Crime/Assets/Scripts/FloatingNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingNumbers : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public int damage;
+    public Text displayNumber;
+    public 
+
+    void Update()
+    {
+        damage = GameObject.Find("Player").GetComponent<PlayerStats>().currentDMG;
+
+        displayNumber.text = "" + damage;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.up, (moveSpeed * Time.deltaTime));
+    }
+}
+
