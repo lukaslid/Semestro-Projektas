415c51a8b5274d0808b6e1bdae21fb68c6229940
Karolis <butnoriusk@gmail.com>
2018-05-07 20:08:57 +0300

    MAP7 completed, camera stops when reach map bounds

--- Havana Crime/Assets/Scripts/CameraController.cs
+++ Havana Crime/Assets/Scripts/CameraController.cs
@@ -7,14 +7,31 @@ public class CameraController : MonoBehaviour
 
     public GameObject player;
     private Vector3 offset;
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
 
     private void Start()
     {
         offset = transform.position - player.transform.position;
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     private void Update()
     {
         transform.position = player.transform.position + offset;
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
