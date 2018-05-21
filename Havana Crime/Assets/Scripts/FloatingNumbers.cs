ec97917f0da4f1f3ba016e08be03db40411c8bd5
Karolis <butnoriusk@gmail.com>
2018-05-21 23:36:57 +0300

    Floating dmg number fix. Changed locations for some map objects

--- Havana Crime/Assets/Scripts/FloatingNumbers.cs
+++ Havana Crime/Assets/Scripts/FloatingNumbers.cs
@@ -7,14 +7,14 @@ public class FloatingNumbers : MonoBehaviour
 
     public float moveSpeed;
     public int damage;
-    public TextMesh text;
+    //public TextMesh text;
     
 
     public void Update()
     {
         //damage = GameObject.Find("Player").GetComponent<PlayerStats>().currentDMG;
         //text.text = damage.ToString();
-        //transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.up, (moveSpeed * Time.deltaTime));
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + transform.up, (moveSpeed * Time.deltaTime));
     }
 }
 
